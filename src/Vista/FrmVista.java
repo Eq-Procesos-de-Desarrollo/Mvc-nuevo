0213570385f0ad0390b646030bfec221aaaefef0
Victor <victorrosas@micorreo.upp.edu.mx>
2022-02-25 17:25:37 -0600

    Seguimiento clientes

--- src/Vista/FrmVista.java
+++ src/Vista/FrmVista.java
@@ -37,7 +37,6 @@ public class FrmVista extends javax.swing.JFrame {
         lblUsuario = new javax.swing.JLabel();
         lblPsw = new javax.swing.JLabel();
         cmdEliminar = new javax.swing.JButton();
-        jTextField1 = new javax.swing.JTextField();
 
         jButton1.setText("jButton1");
 
@@ -54,12 +53,6 @@ public class FrmVista extends javax.swing.JFrame {
         lblPsw.setText("Password");
 
         cmdEliminar.setText("Eliminar");
-
-        jTextField1.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jTextField1ActionPerformed(evt);
-            }
-        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -80,17 +73,11 @@ public class FrmVista extends javax.swing.JFrame {
                 .addGap(18, 18, 18)
                 .addComponent(cmdEliminar)
                 .addContainerGap(95, Short.MAX_VALUE))
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, 83, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(54, 54, 54))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(15, 15, 15)
+                .addGap(50, 50, 50)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(txtNombre, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(lblNombre))
@@ -111,10 +98,6 @@ public class FrmVista extends javax.swing.JFrame {
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
-
-    private void jTextField1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTextField1ActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_jTextField1ActionPerformed
 
     public void arranca() {
        pack();// coloca los componentes
@@ -161,7 +144,6 @@ public class FrmVista extends javax.swing.JFrame {
     public javax.swing.JButton cmdEliminar;
     private javax.swing.JButton jButton1;
     private javax.swing.JButton jButton2;
-    private javax.swing.JTextField jTextField1;
     private javax.swing.JLabel lblNombre;
     private javax.swing.JLabel lblPsw;
     private javax.swing.JLabel lblUsuario;
